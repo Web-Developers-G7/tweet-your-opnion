081d84382fd4afe27afe28a9f4112cc74ad8f95f
AlaaTaima <alaa.ktaima@gmail.com>
2020-02-04 00:07:06 +0200

    edit user-data query to get his posts and data Relates #6

--- server/db/queries/getUserById.js
+++ server/db/queries/getUserById.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
 
 module.exports = id => {
-	return connection.query('SELECT * FROM users WHERE id = $1', [id]);
+	return connection.query('SELECT * FROM users INNER JOIN posts on posts.user_id=users.id WHERE users.id = $1', [id]);
 };
